7bb7db87b9cc39e63db9ec7301c0231ec7d7c2ae
Nick McHale <mchalen87@gmail.com>
2016-11-23 18:22:14 -0800

    Setup basic angular app
    I setup a really basic angular JS app framework

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-#app logic goes here
-#if these changes propoagate correctly, we can be sure the backup 
-#system is automated.
+var app = angular.module("checkout_app", []); 
+app.controller("checkout_ctrl", function($scope) {
+    $scope.items = {
+      poptarts: {
+          item_name: "Pop Tarts",
+          quantity: 50,
+          description: "Delicous breakfast pastry",
+          tags:["snacks", "pantry"]
+      },
+      printer_ink: {
+          item_name: "Printer Ink",
+          quantity: 100,
+          description: "Black printer ink for HP-1050",
+          tags:["ink", "printer supply"]
+      }
+      
+    };
+});
