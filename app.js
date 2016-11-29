89801b629e3d79ac939bc3ec071f99a644ece737
Nick McHale <mchalen87@gmail.com>
2016-11-28 19:21:13 -0800

    Added Cart to prototype
    This is a bit broken!

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 var app = angular.module("checkout_app", []); 
-app.controller("checkout_ctrl", function($scope) {
+app.controller("checkout_ctrl", function ($scope) {
     $scope.items = {
-      poptarts: {
+      0:{
+          id: 0,
           item_name: "Pop Tarts",
           quantity: 50,
           description: "Delicous breakfast pastry",
           tags:["snacks", "pantry"]
       },
-      printer_ink: {
+      1:{
+          id: 1,
           item_name: "Printer Ink",
           quantity: 100,
           description: "Black printer ink for HP-1050",
           tags:["ink", "printer supply"]
-      }
-      
+      }      
+        };
+    
+    $scope.cart = [];
+    
+    $scope.addToCart = function($scope, id){
+       # $scope.cart.push({item_name: $scope.items[id].item_name, quantity: 1});
+        $scope.items[id].quantity -= 1;
+    };
+    
+    $scope.removeFromCart = function($scope, item){
+        delete item;
+    };
+    
+    $scope.checkout = function($scope){
+        console.log("CHeckout complete");
     };
 });
