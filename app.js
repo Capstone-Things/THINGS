1e6ecb3f4883738dfd019e19055eda00c327577c
Nick McHale <mchalen87@gmail.com>
2016-11-29 00:19:54 -0800

    Fixed add to cart
    Implimented Add to cart functionality

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 var app = angular.module("checkout_app", []); 
-app.controller("checkout_ctrl", function ($scope) {
-    $scope.items = {
-      0:{
-          id: 0,
-          item_name: "Pop Tarts",
-          quantity: 50,
-          description: "Delicous breakfast pastry",
-          tags:["snacks", "pantry"]
-      },
-      1:{
-          id: 1,
-          item_name: "Printer Ink",
-          quantity: 100,
-          description: "Black printer ink for HP-1050",
-          tags:["ink", "printer supply"]
-      }      
-        };
+app.controller("checkout_ctrl", function ($scope, $log) {
+    
+    $scope.items = [
+        {
+            id: 0,
+            item_name: "Pop Tarts",
+            quantity: 12,
+            description: "Delicous breakfast pastry",
+            tags: ["snacks", "pantry"]
+        },
+        {
+            id: 1,
+            item_name: "Printer Ink",
+            quantity: 4,
+            description: "Black printer ink for HP-1050",
+            tags: ["ink", "printer supply"]
+        }
+    ];
     
     $scope.cart = [];
     
-    $scope.addToCart = function($scope, id){
-       # $scope.cart.push({item_name: $scope.items[id].item_name, quantity: 1});
-        $scope.items[id].quantity -= 1;
+    $scope.addToCart = function (id) {
+        var index = -1, itemArr = $scope.items, i = 0;
+		for (i = 0; i < $scope.items.length; i = i + 1) {
+			if (itemArr[i].id === id) {
+				index = i;
+                $scope.cart.push({
+                    id: $scope.items[i].id,
+                    item_name: $scope.items[i].item_name,
+                    quantity: 1
+                });
+				$scope.items[i].quantity -= 1;
+                break;
+			}
+		}
+		if (index === -1) {
+			$scope.$log("Something gone wrong");
+        }
     };
     
-    $scope.removeFromCart = function($scope, item){
-        delete item;
+    $scope.removeFromCart = function () {
+        
     };
     
-    $scope.checkout = function($scope){
-        console.log("CHeckout complete");
+    $scope.checkout = function () {
+        $scope.log("Checkout complete");
     };
 });
