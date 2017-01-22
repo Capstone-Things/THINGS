ef4ca693f2550c3d3917b364af548368bd9f4240
Manpreet Bahl <manpreet@pdx.edu>
2017-01-21 19:41:31 -0800

    Add/Remove to Cart added.

--- Front_End/templates/js/app.js
+++ Front_End/templates/js/app.js
@@ -34,12 +34,41 @@ function LoginCheckController($scope, $location) {
 }
 
 
+
 //Original Controller
 app.controller("catthings_ctrl", function ($scope, $window) {
- 
+
 });
 
+app.controller('InventoryController', ['$scope', '$http', InventoryController]);
+function InventoryController($scope, $http) {
 
+  //Get latest inventory data from database
+  $http.get('/view').then(function (response) {
+      $scope.inventory = response.data;
+  });
+
+  $scope.cart=[];
+
+  //Add to cart
+  $scope.addToCart = function(item) {
+    item.checked = false;
+    $scope.cart.push(item);
+  };
+
+  //Remove selected items from cart
+  $scope.removeSelected = function() {
+    var newCart=[];
+        angular.forEach($scope.cart,function(item){
+        if(!item.checked){
+            newCart.push(item);
+        }
+    });
+    $scope.cart=newCart;
+  }
+}
+
+/* Old inventory controller
 //Mock API Call Test
 app.controller('Inventory', function Inventory($http) {
     var ctrl = this;
@@ -53,13 +82,17 @@ app.controller('Inventory', function Inventory($http) {
 
     ctrl.getInventory();
 });
+*/
 
 app.run(function ($httpBackend) {
     var inventory = [{name: 'Pop Tarts', description: 'Yummy', quantity: '5'}, {name: 'Kool-Aid', description: 'Oh Yeah', quantity: '10'}, {name: 'Printer Ink', description: 'Ink for printer', quantity: '30'}];
 
-  // returns the current inventory list
+    //returns the current inventory list
     $httpBackend.whenGET('/view').respond(inventory);
-    
+
+    //checkout items
+    //$httpBackend.whenPost('/checkout').respond();
+
     $httpBackend.whenGET('templates/html/login.html').passThrough();
     //$httpBackend.whenGET('/templates/html/login.html').passThrough();
     $httpBackend.whenGET('templates/html/home.html').passThrough();
