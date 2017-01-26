f6d5fefcb8d30fefa7d28d924b0d4a38b2194fc1
Luke Kazmierowicz <luke24>
2017-01-26 10:20:49 -0800

    Get multiple parameters passed in URL and inserted in Database Query

--- API/things-api/app.js
+++ API/things-api/app.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/items', function(req, res){
+app.get('/items/:id/:qty', function(req, res){
   //first query the database
   //then return the results to the user
 
@@ -16,16 +16,19 @@ app.get('/items', function(req, res){
     if(err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query('SELECT * FROM items', [], function(err, result) {
-    //call `done()` to release the client back to the pool
+    client.query('SELECT * FROM items WHERE item_id = $1 AND quantity > $2', [req.params.id, req.params.qty], function(err, result) {
+      //call `done()` to release the client back to the pool
       done();
 
       if(err) {
         return console.error('error running query', err);
       }
-      console.log(result.rows[0].number);
+      console.log(req.params.id);
+      console.log(req.params.qty);
+
       //output: 1
       res.send(result.rows);
+
   });
 });
 
