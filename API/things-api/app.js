f28b8016b79ec1f30eaa55a86e2ddd97a0ee5632
Luke Kazmierowicz <luke24>
2017-01-28 23:31:41 -0800

    First version of Check in / Check out function

--- API/things-api/app.js
+++ API/things-api/app.js
@@ -29,11 +29,39 @@ app.get('/items/:id/:qty', function(req, res){
       //output: 1
       res.send(result.rows);
 
+    });
   });
 });
 
+//Checkout / Check in items
+//------------------------------------------------------------------------------
+app.get('/transaction/:id/:person/:qty', function(req, res){
+  //first query the database
+  //then return the results to the user
 
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('INSERT INTO transactions(item_id, person, qty_changed) VALUES ($1, $2, $3)', [req.params.id, req.params.person, req.params.qty], function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        res.send(err);
+        return console.error('error running query', err);
+      } else {
+        res.send('Transaction Completed Successfully');
+      }
+      console.log(req.params.id);
+      console.log(req.params.qty);
+      console.log(req.params.person);
+
+      //output: 1
+    });
+  });
 });
+
 
 app.listen(3000, function () {
   console.log('Listening on port 3000');
