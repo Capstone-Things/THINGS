02041a4177d79a9e40235854d79723eba2b7dc7e
Luke Kazmierowicz <luke24>
2017-02-07 16:01:26 -0800

    Add API route for tagging items
    The item must already exist in the database and the tag name must be
    unique.

--- API/things-api/app.js
+++ API/things-api/app.js
@@ -28,7 +28,7 @@ app.get('/checkout/:id/:person/:qty', function(req, res) {
 * /params   id
 *           person
 *           qty
-* /brief    Route to add item back into inventory
+* /brief    Route to add quantity to an existing item in the inventory
 *
 * /author   Luke
 ****************************************************/
@@ -93,7 +93,10 @@ app.get('/view', function(req, res){
 
 /****************************************************
 * /path     /add/:name/:desc/:price/:thresh
-* /params
+* /params   name - the name of the item
+*           desc - the item description
+*           price - the price of the item in decimal form
+*           thresh - the threshold to notify the admin if the quantity drops below
 *
 * /brief    Add new item api route, used to insert rows into
 *
@@ -113,6 +116,35 @@ app.get('/add/:name/:desc/:price/:thresh', function(req, res){
             //call `done()` to release the client back to the pool
             done();
             errResultHandler(err, 'Item Added Successfully', res);
+        });
+    });
+});
+
+
+
+/****************************************************
+* /path     /tagitem/:id/:tag
+* /params   :id - The id of the item to tag
+*           :tag - the name of the tag to give the item
+*
+* /brief    Add a tag to an item given its id
+*
+* /author   Luke
+* /date     2/7/2017
+****************************************************/
+app.get('/tagitem/:id/:tag', function(req, res){
+  //first query the database
+  //then return the results to the user
+
+      pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('INSERT INTO tags VALUES ($1, $2)',
+                    [req.params.tag, req.params.id], function(err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+            errResultHandler(err, 'Tag Added Successfully', res);
         });
     });
 });
@@ -206,8 +238,10 @@ app.get('/stats/range/:start_date/:end_date', function(err, client, done) {
 
 /****************************************************
 * /path     /stats/range/:start_date/:end_date
-* /params   :start_date - Beginning of time frame
-*           :end_date - End of time frame
+* /params   :err - the error that occured, null if none
+*           :result - the result of the database query
+                      (either the rows returned or success message)
+            :res - the function to handle the responding the result
 *
 * /brief    Responds with a meaningful status and returns the results
 *
