1d103125657f2097f6ffab479aff18cb6768b78c
Nick McHale <nmchale@pdx.edu>
2017-02-12 18:01:02 -0800

    Mostly finished with restructure.
    I have moved all of the routes to the new format, with the exception of
    History/stats.  I think we may need to revise our stategy on these
    routes and how we handle their parameters.

--- API/things-api/routes/a/admin/index.js
+++ API/things-api/routes/a/admin/index.js
@@ -3,6 +3,9 @@
 // https://scotch.io/tutorials/keeping-api-routing-clean-using-express-routers
 const routes = require('express').Router();
 const checkin = require('./checkin');
+const addNewItem = require('./add_new_item');
+const addTag = require('./add_tag');
+const shoppingList = require('./shopping_list');
 
 //add route handelers for subfolders here:
 
@@ -10,6 +13,9 @@ const checkin = require('./checkin');
 
 //add route handelers for this directorys routes here:
 routes.post('/checkin/:id/:person/:qty', checkin);
+routes.put('/add/:name/:desc/:price/:thresh', addNewItem);
+routes.post('/tagitem/:id/:tag', addTag);
+routes.get('/shopping_list', shoppingList);
 //this is an inline route handler...
 //this is where you land if you goto GET https://localhost:3000/a/admin/
 routes.get('/', (req, res) => {
---
+++ API/things-api/routes/a/admin/shopping_list.js
@@ -0,0 +1,23 @@
+/****************************************************
+* /path     /shoppinglist
+* /params   null
+* /brief    Returns all items with quantity less than threshold
+*
+* /author   Luke
+****************************************************/
+module.exports=(req,res) => {
+  //first query the database
+  //then return the results to the user
+
+      res.app.locals.pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT item_name AS name, description, price FROM items WHERE quantity < threshold', [], function(err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+            res.app.locals.helpers.errResultHandler(err, result.rows, res);
+        });
+    });
+
+};
