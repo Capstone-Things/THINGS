892e8a88f018dd6e090501fc80e79fbf279a67ee
Manpreet Bahl <manpreet@pdx.edu>
2017-02-20 11:21:46 -0800

    Custom searchable datatable

--- Front_End/templates/js/controllers/checkin_controller.js
+++ Front_End/templates/js/controllers/checkin_controller.js
@@ -2,8 +2,18 @@ var app = angular.module("catthings_app");
 
 app.controller('CheckInController', ['$scope', '$http',  '$location', '$rootScope', 'inventoryList', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'thingsAPI', CheckInController]);
 function CheckInController($scope, $http,  $location, $rootScope, inventoryList, DTOptionsBuilder, DTColumnDefBuilder, thingsAPI){
+  $scope.searchQuery = '';
 
   $scope.dtOptions = DTOptionsBuilder.newOptions().withOption('sDom', 'rtip');
+  $scope.dtColumns = [
+    DTColumnDefBuilder.newColumnDef(0),
+    DTColumnDefBuilder.newColumnDef(1).withOption('searchable', false),
+    DTColumnDefBuilder.newColumnDef(2).withOption('searchable', false)
+  ]
+
+  $scope.dtInstanceCallback = function(dtInstance){
+    $scope.dtInstance = dtInstance;
+  };
 
   //Get latest inventory data from database
   thingsAPI.getView().then(function (response) {
@@ -11,4 +21,15 @@ function CheckInController($scope, $http,  $location, $rootScope, inventoryList,
       $scope.stuff=inventoryList.getInventory();
       console.log($scope.stuff);
   });
+
+  $scope.searchTable = function(){
+    if($scope.searchQuery == null){
+      $scope.dtInstance.DataTable.search("").draw();
+    }
+    else{
+      $scope.dtInstance.DataTable.search($scope.searchQuery);
+      $scope.dtInstance.DataTable.search($scope.searchQuery).draw();
+    }
+  };
+
 }
