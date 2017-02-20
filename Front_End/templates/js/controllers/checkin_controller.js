bc72827a27bb76aa3c6eab7859871a8b008f2a96
Manpreet Bahl <manpreet@pdx.edu>
2017-02-20 12:17:57 -0800

    Saving stuff

--- Front_End/templates/js/controllers/checkin_controller.js
+++ Front_End/templates/js/controllers/checkin_controller.js
@@ -2,25 +2,20 @@ var app = angular.module("catthings_app");
 
 app.controller('CheckInController', ['$scope', '$http',  '$location', '$rootScope', 'inventoryList', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'thingsAPI', CheckInController]);
 function CheckInController($scope, $http,  $location, $rootScope, inventoryList, DTOptionsBuilder, DTColumnDefBuilder, thingsAPI){
+  $scope.checkin=[];
   $scope.searchQuery = '';
+  $scope.dtOptions = DTOptionsBuilder.newOptions().withOption('sDom', 'rtip', 'select');
 
-  $scope.dtOptions = DTOptionsBuilder.newOptions().withOption('sDom', 'rtip');
   $scope.dtColumns = [
-    DTColumnDefBuilder.newColumnDef(0),
-    DTColumnDefBuilder.newColumnDef(1).withOption('searchable', false),
-    DTColumnDefBuilder.newColumnDef(2).withOption('searchable', false)
+    DTColumnDefBuilder.newColumnDef(0).withOption('searchable', false),
+    DTColumnDefBuilder.newColumnDef(1),
+    DTColumnDefBuilder.newColumnDef(2).withOption('searchable', false),
+    DTColumnDefBuilder.newColumnDef(3).withOption('searchable', false)
   ]
 
   $scope.dtInstanceCallback = function(dtInstance){
     $scope.dtInstance = dtInstance;
   };
-
-  //Get latest inventory data from database
-  thingsAPI.getView().then(function (response) {
-      inventoryList.setInventory(response.data);
-      $scope.stuff=inventoryList.getInventory();
-      console.log($scope.stuff);
-  });
 
   $scope.searchTable = function(){
     if($scope.searchQuery == null){
@@ -31,5 +26,24 @@ function CheckInController($scope, $http,  $location, $rootScope, inventoryList,
       $scope.dtInstance.DataTable.search($scope.searchQuery).draw();
     }
   };
+
+  //Get latest inventory data from database
+  thingsAPI.getView().then(function (response) {
+      inventoryList.setInventory(response.data);
+      $scope.stuff=inventoryList.getInventory();
+      console.log($scope.stuff);
+  });
+
+  function check(){
+    if(!$scope.cart.length){
+      $scope.cartNotEmpty = false;
+      $scope.cartEmpty = true;
+    }
+    else{
+      $scope.cartNotEmpty = true;
+      $scope.cartEmpty = false;
+    }
+  }
+  check()
 
 }
