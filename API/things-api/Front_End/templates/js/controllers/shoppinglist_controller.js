4f570737118437dbd82750773bb4a132419a8205
chenhil <chn.hillman@gmail.com>
2017-03-10 00:09:05 -0800

    Styled the datatable with bootstrap

--- API/things-api/Front_End/templates/js/controllers/shoppinglist_controller.js
+++ API/things-api/Front_End/templates/js/controllers/shoppinglist_controller.js
@@ -3,8 +3,10 @@ var app = angular.module("catthings_app");
 app.controller('ShoppingListController', ['$scope', '$http', ShoppingListController]);
 function ShoppingListController($scope, $http) {
   //Get latest inventory data from database
-  $http.get("https://things.cs.pdx.edu:3000/shoppinglist")
+  $http.get("https://things.cs.pdx.edu:3000/api/shoppinglist")
   .success(function (data) {
       $scope.shoppingList = data;
+  }, function (err) { 
+      console.log(err);
   });
 }
