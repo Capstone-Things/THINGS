3acd8b27448415afcb3c2a04c252dadd786afc88
Manpreet Bahl <manpreet@pdx.edu>
2017-03-12 18:16:08 -0700

    Success/Failure message added to Request item page

--- API/things-api/Front_End/templates/js/controllers/request_controller.js
+++ API/things-api/Front_End/templates/js/controllers/request_controller.js
@@ -2,27 +2,20 @@ var app = angular.module("catthings_app");
 
 
 //Request Controller
-app.controller('RequestController', ['$scope', '$http', '$location', 'thingsAPI', RequestController]);
-function RequestController($scope, $http, $location, thingsAPI){
+app.controller('RequestController', ['$scope', '$http', '$location', '$window', 'thingsAPI', RequestController]);
+function RequestController($scope, $http, $location, $window, thingsAPI){
   $scope.question = {};
 
   //sendRequest
   $scope.sendRequest = function() {
     var qData = $scope.question;
-    console.log(qData);
     thingsAPI.request(qData).then(function(response){
       if(response.status === 200){
-        $location.path("home");
+        $window.alert("Request submitted succesfully");
       }
-    })
-    /*
-    $http.post('https://localhost:3000/request', qData).then(function(response){
-      console.log(response.status);
-      if(response.status === 200){
-              $location.path("home");
+      else{
+        $window.alert("Unable to submit request. Please try again or contact an administrator");
       }
-      //Else 404 error....
     });
-    */
-  }
+  };
 }
