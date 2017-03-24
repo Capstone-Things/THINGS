be4b8f12463ecc8c437fbc24e9b058335b3cebe7
Nick McHale <nmchale@pdx.edu>
2017-03-24 01:12:01 -0700

    I put the httpbackend back in
    removing http backend made things break.

--- API/things-api/Front_End/templates/js/app.js
+++ API/things-api/Front_End/templates/js/app.js
@@ -60,5 +60,49 @@ app.config(function($stateProvider, $urlRouterProvider, $sceDelegateProvider) {
    ]);
 });
 
+app.run(function ($httpBackend) {
+    var inventory = [{name: 'Pop Tarts', description: 'Yummy', quantity: '5'}, {name: 'Kool-Aid', description: 'Oh Yeah', quantity: '10'}, {name: 'Printer Ink', description: 'Ink for printer', quantity: '30'}];
 
-app.run(function () {});
+    //returns the current inventory list
+    $httpBackend.whenGET('/view').respond(inventory);
+
+    //Checkout items
+    $httpBackend.whenPOST('/checkout').respond(function(method, url, data) {
+        data = JSON.parse(data);
+        for (var i = 0; i < data.length; i++){
+          for (var j = 0; j < inventory.length; j++){
+            if (inventory[j].name === data[i].name){
+              console.log(inventory[j].quantity);
+              inventory[j].quantity = (parseInt(inventory[j].quantity) - data[i].quantity).toString();
+              console.log(inventory[j].quantity);
+              break;
+            }
+          }
+        }
+        return [200, {}, {}];
+    });
+
+    $httpBackend.whenGET('templates/html/login.html').passThrough();
+    $httpBackend.whenGET('templates/html/home.html').passThrough();
+    $httpBackend.whenGET('templates/html/cart.html').passThrough();
+    $httpBackend.whenGET('templates/html/request.html').passThrough();
+    $httpBackend.whenGET('templates/html/shoppinglist.html').passThrough();
+
+    $httpBackend.whenGET('templates/html/bootstrapNavbar.html').passThrough();
+    $httpBackend.whenGET('templates/html/statistic.html').passThrough();
+
+    $httpBackend.whenGET('templates/html/checkIn.html').passThrough();
+    $httpBackend.whenGET('templates/html/promptQuantity.html').passThrough();
+
+    $httpBackend.whenGET('templates/html/viewhistory.html').passThrough();
+    $httpBackend.whenGET('templates/html/additem.html').passThrough();
+
+    $httpBackend.whenGET("http://localhost:3000/view").passThrough();
+
+    $httpBackend.whenJSONP(/https:\/\/things\.cs\.pdx\.edu:3000\/*/).passThrough();
+    $httpBackend.whenGET(/https:\/\/things\.cs\.pdx\.edu:3000\/*/).passThrough();
+    $httpBackend.whenPOST(/https:\/\/things\.cs\.pdx\.edu:3000\/*/).passThrough();
+    $httpBackend.whenPOST(/https:\/\/localhost:3000\/*/).passThrough();
+    $httpBackend.whenGET(/https:\/\/localhost:3000\/*/).passThrough();
+
+});
